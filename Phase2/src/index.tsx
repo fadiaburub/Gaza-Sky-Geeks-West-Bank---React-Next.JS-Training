50c51851133cf58a7080b5e0f9c8e41509ff483d
Fadi Aburub <faburub@pma.ps>
2024-09-30 09:10:41 +0300

    Add Phase2 project files

---
+++ Phase2/src/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+
+  // Performance measuring
+  reportWebVitals();
+} else {
+  console.error("Root element not found!");
+}
